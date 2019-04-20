fc3071049b1c8f5baab1ed681a722a60a191b044
Fatihalparslan <fatih22alparslan@gmail.com>
2019-04-21 00:25:27 +0300

    ESP8266 wifi modülünde kalıcı wifi şifresi kaydetme amaçlı bir çalışma
    Bu projede Arduino IDEsi kullanılmıştır. Çalışmada ESP8266  modülünün eepromunda bir şifre ve password bilgisi varsa bunların kullanılarak wifiye bağlanılması amaçlanmıştır. Eğer şifre yoksa veya yüz saniyeden uzun wifiye bağlanılmamışsa  esp modülü access point moduna geçer ve 192.168.4.1 adresine gönderilecek ssid#password mesajını analiz edip wifi adı ve şifreyi kaydedip tekrar wifiye bağlanmaya çalışır.

---
+++ eepromaccespoint/eepromaccespoint.ino
@@ -0,0 +1,119 @@
+
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
+WiFiServer wifiServer(80);//wifi server 80 porttan çalışacak
+int status;//wifinin bağlı olma durumunu gösterecek değişken
+int i=0;//stamod fonksiyonundaki for döngüsü için kullanılacak
+int j=0;//wifi bağlantısı kontrolü için oluşturulan for döngüsünde kullanılmıştır.
+bool apmoddurum,stamoddurum,conn=false,degis=false;//
+
+const char *APssid = "esp_erisim";
+const char *APpassword = "espsifre";
+
+char ssid[30] = "";
+char password[30]= "";
+
+void stamod(){
+  for(i=0;i<100;i++){
+  delay(1000);
+  }
+WiFi.disconnect();
+WiFi.mode(WIFI_STA);
+WiFi.begin(ssid,password);
+  }
+
+void setup() {
+Serial.begin(115200);
+delay(1000);
+EEPROM.begin(512);
+read_string(30,0).toCharArray(ssid,30); 
+read_string(30,100).toCharArray(password,30); 
+for(j=0;j<10;j++){
+     status = WiFi.begin(ssid, password);
+    // wait 10 seconds for connection:
+    delay(1000);
+}
+if (status==true){
+  stamod();
+  }else{
+ Serial.println(conn);
+Serial.println(ssid);
+Serial.println(password);
+apmoddurum=WiFi.softAP(APssid, APpassword);
+stamoddurum=false;
+  }
+  Serial.println(conn);
+Serial.println(ssid);
+Serial.println(password);
+//stamod();
+ wifiServer.begin();
+}
+
+void loop() {
+ if(apmoddurum){
+ 
+  char wifiadi[30]="";
+  char pass[30]="";
+  WiFiClient client = wifiServer.available();
+ 
+  if (client) {
+ 
+    while (client.connected()) {
+ 
+      while (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+  if(c=='#'){
+    degis==true;
+    i=0;
+    }
+  
+ if(degis==false){
+    wifiadi[i]=c;
+    i++;
+    }else{
+      pass[i]=c;
+      i++;
+    }
+    }
+    }
+    degis=false;
+    i=0;
+    
+    client.stop();
+  write_to_Memory(String(wifiadi),String(pass));
+ 
+  }
+  
+  }
+  
+}
+void write_to_Memory(String s, String p) {
+  s += ";";
+  write_EEPROM(s, 0);
+  p += ";";
+  write_EEPROM(p, 100);
+  EEPROM.commit();
+}
+//write to memory
+void write_EEPROM(String x, int pos) {
+  for (int n = pos; n < x.length() + pos; n++) {
+    EEPROM.write(n, x[n - pos]);
+  }
+}
+//Reads a string out of memory
+String read_string(int l, int p){
+  String temp;
+  for (int n = p; n < l+p; ++n)
+    {
+     if(char(EEPROM.read(n))!=';'){
+        if(isWhitespace(char(EEPROM.read(n)))){
+          //do nothing
+        }else temp += String(char(EEPROM.read(n)));
+      
+     }else n=l+p;
+     
+    }
+  return temp;
+}
+
